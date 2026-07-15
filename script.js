32929c50399cbc4fa1bfc60598e0862035c92d36
ajax <ethinruesch@gmail.com>
2026-07-15 14:28:20 -0600

    Add attempt counter to web game

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const secretNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
 
 const input = document.getElementById("guessInput");
 const button = document.getElementById("guessButton");
@@ -6,12 +7,13 @@ const message = document.getElementById("message");
 
 button.addEventListener("click", function () {
     const guess = Number(input.value);
+    attempts = attempts + 1;
 
     if (guess < secretNumber) {
         message.textContent = "Too low!";
     } else if (guess > secretNumber) {
         message.textContent = "Too high!";
     } else {
-        message.textContent = "You got it! The number was " + secretNumber;
+        message.textContent = "You got it! The number was " + secretNumber + ". It took you " + attempts + " tries.";
     }
 });
